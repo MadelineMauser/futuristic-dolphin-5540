b662568b9d707e9b07ae5b2ab3788467ed8cd501
Madeline Mauser <madeline.mauser@gmail.com>
2022-09-12 11:53:01 -0600

    Add test for #ordered_rides

--- app/controllers/mechanic_rides_controller.rb
+++ app/controllers/mechanic_rides_controller.rb
@@ -1,4 +1,6 @@
 class MechanicRidesController < ApplicationController
   def show
+    @mechanic = Mechanic.find(params[:id])
+    @ordered_rides = @mechanic.ordered_rides
   end
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -16,4 +16,25 @@ RSpec.describe Mechanic, type: :model do
       end
     end
   end
+  describe 'instance methods' do
+    describe '#ordered_rides' do
+      it 'returns a list of open rides that the mechanic is assigned in descending order of thrill rating' do
+        @kara = Mechanic.create!(name: 'Kara Smith', years_experience: 11)
+        @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+        @universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
+
+        @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+        @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+        @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+        @jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+
+        @assignment1 = MechanicRide.create!(mechanic_id: @kara.id, ride_id: @hurler.id)
+        @assignment2 = MechanicRide.create!(mechanic_id: @kara.id, ride_id: @scrambler.id)
+        @assignment3 = MechanicRide.create!(mechanic_id: @kara.id, ride_id: @ferris.id)
+        @assignment4 = MechanicRide.create!(mechanic_id: @kara.id, ride_id: @jaws.id)
+
+        expect(@kara.ordered_rides).to eq([@hurler, @jaws, @scrambler])
+      end
+    end
+  end
 end
