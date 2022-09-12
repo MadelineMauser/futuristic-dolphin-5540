6ce915d397082c1b0d0e4949b1f8efcdf4707f5b
Madeline Mauser <madeline.mauser@gmail.com>
2022-09-12 09:19:34 -0600

    Fix typo in test for Mechanic

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Mechanic, type: :model do
   describe 'relationships' do
-    it { should have_many(:mechanicrides) }
-    it { should have_many(:rides).through(:mechanicrides) }
+    it { should have_many(:mechanic_rides) }
+    it { should have_many(:rides).through(:mechanic_rides) }
   end
 end
