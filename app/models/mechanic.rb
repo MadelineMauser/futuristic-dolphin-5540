9244de564c75749d1c8376bc5043436a864eb54d
Madeline Mauser <madeline.mauser@gmail.com>
2022-09-12 11:56:25 -0600

    Add #ordered_rides

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -5,4 +5,8 @@ class Mechanic < ApplicationRecord
   def self.average_years_of_experience
     all.pluck(:years_experience).sum.to_f / self.all.size
   end
+
+  def ordered_rides
+    rides.where(open: true).order(thrill_rating: :desc)
+  end
 end
