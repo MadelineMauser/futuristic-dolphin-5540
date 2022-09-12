03e492ca054e3fed9ec22e4fec223423ae4fe4a8
Madeline Mauser <madeline.mauser@gmail.com>
2022-09-12 09:47:19 -0600

    Add mechanics to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,6 @@ Ride.destroy_all
 @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
 
 @jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+
+@kara = Mechanic.create!(name: 'Kara Smith', years_experience: 11)
+@fred = Mechanic.create!(name: 'Fred Boat', years_experience: 6)
