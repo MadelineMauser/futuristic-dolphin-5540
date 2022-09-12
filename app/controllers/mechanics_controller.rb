20339c446dd00059334c199426ad2ec3d352d024
Madeline Mauser <madeline.mauser@gmail.com>
2022-09-12 10:36:14 -0600

    Create index view and add mechanic test

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -1,4 +1,6 @@
 class MechanicsController < ApplicationController
   def index
+    @mechanics = Mechanics.all
+    @average_yoe = Mechanics.average_years_of_experience
   end
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Mechanic, type: :model do
     it { should have_many(:mechanic_rides) }
     it { should have_many(:rides).through(:mechanic_rides) }
   end
+
+  describe 'class methods' do
+    describe '#average_years_of_experience' do
+      it 'returns the average years of experience of all mechanics' do
+        @kara = Mechanic.create!(name: 'Kara Smith', years_experience: 11)
+        @fred = Mechanic.create!(name: 'Fred Boat', years_experience: 6)
+
+        expect(Mechanic.average_years_of_experience).to eq(8.5)
+      end
+    end
+  end
 end
